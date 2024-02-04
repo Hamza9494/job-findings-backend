64cecb6b7f8333748fa3e0bda93dc93ed9a29e2d
arthur_wsl <baitichhamza@gmail.com>
2024-02-04 12:35:23 +0100

    test

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With , Authorization");
 header('Content-Type: application/json; charset=utf-8');
+
 
 require "./vendor/autoload.php";
 
@@ -11,4 +12,24 @@ use Firebase\JWT\Key;
 
 $headers = getallheaders();
 
-var_dump($headers["Authorization"]);
+$bearer_auth = explode("Bearer ", $headers["Authorization"]);
+
+$jwt_token = $bearer_auth[1];
+
+$key = "mykey2014";
+
+
+$decoded = JWT::decode($jwt_token, new Key($key, "HS256"));
+
+$decoded = (array) $decoded;
+
+
+$email = $decoded["user_email"];
+$mysqli = require __DIR__ . "/database.php";
+$sql = "SELECT * FROM users WHERE email = ? ";
+$stmt = $mysqli->prepare($sql);
+$stmt->bind_param("s", $email);
+$stmt->execute();
+$result = $stmt->get_result();
+$user = $result->fetch_assoc();
+var_dump($user);
